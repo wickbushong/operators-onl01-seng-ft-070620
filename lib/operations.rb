18fbc99bfa2f36c092f181d3693872571a7240f3
Wick Bushong <wickbushong@gmail.com>
2020-07-06 13:51:30 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,15 @@
 def unsafe?(speed)
-
+ if speed > 60
+   true
+ elsif speed < 40
+   true
+ else
+   false
+ end
 end
 
 
 
 def not_safe?(speed)
-	
+  40 < speed && speed < 60 ? false : true
 end
-	
-
-
